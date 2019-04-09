91ee182e085917ee0b54d4f0e10d31fd3c7005b1
Kritsia Figueroa <kritsiafigueroa@yahoo.com>
2019-04-08 21:35:06 -0400

    Added some code to Savings Account test under "add interest"

--- src/test/java/banking/SavingsAccountTest.java
+++ src/test/java/banking/SavingsAccountTest.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SavingsAccountTest {
+    private static double DEFAULT_INTEREST_RATE = 2.0;
 
     @Test
-    public void SavingsAccount() {
+    public void SavingsAccount(double initialBalance) {
+        
     }
 
     @Test
-    public void addInterest() {
+    public void addInterest(double balance, double initialBalance) {
+        balance = initialBalance + (initialBalance * DEFAULT_INTEREST_RATE);
     }
 }
