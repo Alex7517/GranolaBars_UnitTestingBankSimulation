fbf0778906bfd8cbaff66ebc48a4c9f19433a62f
Kritsia Figueroa <kritsiafigueroa@yahoo.com>
2019-04-11 16:45:20 -0400

    Added test code to CustomerTest

--- src/test/java/banking/CustomerTest.java
+++ src/test/java/banking/CustomerTest.java
@@ -8,30 +8,45 @@ public class CustomerTest {
 
     @Test
     public void Customer() {
+        Customer testCustomer = new Customer(bankName,"John", "Doe");
     }
 
     @Test
     public void addSavingsAccount() {
+     Customer testCustomer = new Customer(bankName,"John", "Doe");          
+     assertEquals(1000, "Savings Account");                   
+
     }
 
     @Test
     public void getAccount() {
+      Customer testCustomer = new Customer(bankName,"John", "Doe");  
+      Account custAccount =  testCustomer.getAccount(testCustomer.getCustomerID());
+      assertEquals("", custAccount.getAccountID());
     }
 
     @Test
     public void getCustomerAccounts() {
+        Customer testCustomer = new Customer(bankName,"John", "Doe");    
+        testCustomer.getCustomerAccounts();
     }
 
     @Test
     public void removeAccount() {
+        Customer testCustomer = new Customer(bankName,"John", "Doe");
+        testCustomer.removeAccount ( testCustomer.getCustomerID());
     }
 
     @Test
     public void YtdFees() {
+        Customer testCustomer = new Customer(bankName,"John", "Doe");
+        Assert.assertEquals(0.0, testCustomer.YtdFees());
     }
 
     @Test
     public void YtdInterest() {
+        Customer testCustomer = new Customer(bankName,"John", "Doe");
+        Assert.assertEquals(0.0, testCustomer.YtdInterest());
     }
 
 }
